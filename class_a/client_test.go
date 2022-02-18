3e43b1320bb3791b1485874773717d0839343f49
Chris Shields <christopher.shields143@gmail.com>
2022-02-17 19:16:59 -0800

    Add a test for the block heights function

--- class_a/client_test.go
+++ class_a/client_test.go
@@ -10,6 +10,51 @@ import (
 	"time"
 )
 
+func TestClassA_Blocks(t *testing.T) {
+    tests := []struct {
+        desc        string
+        response    string
+        want        Blocks
+    }{
+        {
+            desc: "WhenNonEmpty",
+            response: `{"data":{"updated_at":"2022-02-18T02:51:16.364252395Z","items":[{"signed_at":"2021-01-01T00:20:01Z","height":11565118}],"pagination":null},"error":false,"error_message":"","error_code":0}`,
+            want: Blocks{
+                UpdatedAt: time.Date(2022, 2, 18, 2, 51, 16, 364252395, time.UTC),
+                Items: []struct {
+                    SignedAt time.Time `json:"signed_at"`
+                    Height int `json:"height"`
+                }{{
+                    time.Date(2021, 1, 1, 0, 20, 1, 0, time.UTC),
+                    11565118,
+                }},
+                Pagination: Pagination{},
+            },
+        },
+    }
+    for _, tc := range tests {
+        tc := tc
+        t.Run(tc.desc, func (t *testing.T) {
+		    s := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			    w.WriteHeader(http.StatusOK)
+			    w.Header().Set("Content-Type", "application/json")
+			    _, err := io.WriteString(w, tc.response)
+			    if err != nil {
+				    t.Fatalf("while write response got err: %v", err)
+			    }
+		    }))
+		    defer s.Close()
+		    api := client.New(s.URL, "ckey_test", s.Client())
+		    classA := Client{API: *api}
+		    got, err := classA.Blocks("chain", "start", "end")
+		    if diff := cmp.Diff(got, tc.want); diff != "" || err != nil {
+			    t.Errorf("%v.Blocks(chain, startDate, endDate) has diff (-got/+want)\n: %v", classA, diff)
+			    t.Errorf("%v.Blocks(chain, startDate, endDate) got err: %v want nil", classA, err)
+		    }
+        })
+    }
+}
+
 func TestClassA_GetTokenBalances_WhenValid(t *testing.T) {
 	t.Parallel()
 	tests := []struct {
